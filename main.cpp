cedb754e3656931fb07f586c9be2cce25d26c58b
MikeWen <wenshui2008@126.com>
2024-02-18 01:26:49 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,18 +7,23 @@
 #include <io.h>
 #include <locale.h>
 
-HANDLE g_hChildStd_IN_Rd = NULL;
-HANDLE g_hChildStd_IN_Wr = NULL;
-HANDLE g_hChildStd_OUT_Rd = NULL;
-HANDLE g_hChildStd_OUT_Wr = NULL;
+HANDLE g_hStdin_Child_Rd = NULL;
+HANDLE g_hStdin_Parent_Wr = NULL;
+HANDLE g_hStdout_Parent_Rd = NULL;
+HANDLE g_hStdout_Child_Wr = NULL;
 
 #define MAX_FILEPATH 4096
 TCHAR g_szMyPath[MAX_FILEPATH] = TEXT("");
 size_t g_iMyPathLen = 0;
 
-#define ErrorExit(s) do { printf(s);printf("\r\n"); return 0; } while(0)
+#define ErrorExit(s) do { printf(s); printf("\r\n"); return 0; } while(0)
+#define CLOSEHANDLE(h) if(h) { CloseHandle(h); h = NULL; }
+#define CloseAllPipes() do { CLOSEHANDLE(g_hStdin_Parent_Wr); CLOSEHANDLE(g_hStdout_Parent_Rd); } while(0)
 
-BOOL InitStdOut() 
+#define MAX_COMMAND_LINE 65536
+#define DEFAULT_CONSOLE_COLOR	( FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
+
+static BOOL CreateStdioPipe()
 { 
 	SECURITY_ATTRIBUTES saAttr; 
 
@@ -27,32 +32,35 @@ BOOL InitStdOut()
 	saAttr.bInheritHandle = TRUE; 
 	saAttr.lpSecurityDescriptor = NULL; 
 
-	// Create a pipe for the child process's STDOUT. 
-	if (! CreatePipe(&g_hChildStd_OUT_Rd, &g_hChildStd_OUT_Wr, &saAttr, 0)) 
+	CloseAllPipes();
+
+	// Create a pipe for reading the child process's STDOUT. 
+	if (! CreatePipe(&g_hStdout_Parent_Rd, &g_hStdout_Child_Wr, &saAttr, 0)) 
 		ErrorExit("Stdout pipe creation failed\n"); 
 
 	// Ensure the read handle to the pipe for STDOUT is not inherited.
-	//if ( ! SetHandleInformation(g_hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0) )
-	//	ErrorExit("Stdout SetHandleInformation");
+	if (! SetHandleInformation(g_hStdout_Parent_Rd, HANDLE_FLAG_INHERIT, 0) )
+		ErrorExit("Stdout SetHandleInformation");
 
-	if (! CreatePipe(&g_hChildStd_IN_Rd, &g_hChildStd_IN_Wr, &saAttr, 0)) 
+	// Create a pipe for writing to the child process's STDIN.
+	if (! CreatePipe(&g_hStdin_Child_Rd, &g_hStdin_Parent_Wr, &saAttr, 0)) 
 		ErrorExit("Stdin CreatePipe"); 
 
 	// Ensure the write handle to the pipe for STDIN is not inherited. 
-	//if ( ! SetHandleInformation(g_hChildStd_IN_Wr, HANDLE_FLAG_INHERIT, 0) )
-	//	ErrorExit("Stdin SetHandleInformation"); 
+	if ( ! SetHandleInformation(g_hStdin_Parent_Wr, HANDLE_FLAG_INHERIT, 0) )
+		ErrorExit("Stdin SetHandleInformation"); 
 
 	return 1;
 }
 
 //
-// cat
+// 
 // Remove ..\ from path 
-// ie: c:\abc\bcd\cde\efg\..\..\..\fgh\ghi.ext = c:\abc\fgh\ghi.ext
-// nTextLength - the length of return string
+// ie: c:\abc\bcd\cde\efg\..\..\..\fgh\ghi.ext => c:\abc\fgh\ghi.ext
+// nPathLength - the length of return string
 // the return string is allocated by malloc
 
-LPTSTR FixPathFileNameString(LPCTSTR pszPath,int &nTextLength)
+static LPTSTR FixRelativePath(LPCTSTR pszPath, int &nOutputLength)
 {
 	LPCTSTR pszPathEnd,pszNameBegin,pszNameEnd;
 	LPTSTR pszReturn = NULL;
@@ -64,7 +72,7 @@ LPTSTR FixPathFileNameString(LPCTSTR pszPath,int &nTextLength)
 
 	if (!pszPathEnd) {
 		pszReturn = _tcsdup(pszPath);
-		nTextLength = (int)_tcslen(pszReturn);
+		nOutputLength = (int)_tcslen(pszReturn);
 		return pszReturn;
 	}
 
@@ -103,55 +111,257 @@ LPTSTR FixPathFileNameString(LPCTSTR pszPath,int &nTextLength)
 			nPathLength ++;
 		}
 		memcpy(pszReturn + nPathLength,pszNameBegin, sizeof (TCHAR) * nFileNameLength);
-		nTextLength = nPathLength + nFileNameLength;
-		pszReturn[nTextLength] = _T('\0');
+		nOutputLength = nPathLength + nFileNameLength;
+		pszReturn[nOutputLength] = _T('\0');
 	}
 
 	return pszReturn;
 }
 
-#define MAX_COMMAND_LINE 65536
+static HANDLE process_job_handle = NULL;
+
+int  CreateChildProcessJob()
+{
+	process_job_handle = CreateJobObject(NULL, NULL);
+
+	if (process_job_handle) {
+		JOBOBJECT_EXTENDED_LIMIT_INFORMATION limit_info;
+		BOOL set_auto_kill_ok;
+
+		memset(&limit_info, 0x0, sizeof(limit_info));
+		limit_info.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE;
+		set_auto_kill_ok = SetInformationJobObject(
+			process_job_handle, 
+			JobObjectExtendedLimitInformation, 
+			&limit_info, 
+			sizeof(limit_info)
+			);
+		if (!set_auto_kill_ok) {
+			CloseHandle(process_job_handle);
+			process_job_handle = NULL;
+
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+static
+void DestroyChildProcessJob()
+{
+	if (process_job_handle) {
+		CloseHandle(process_job_handle);
+		process_job_handle = NULL;
+	}
+}
+
+//
+//
+// return 0: no error
+// otherwise: error code
+//
+
+static
+BOOL CreateChildProcess(HANDLE *pChildHandle, DWORD *pid, BOOL bPrintLog, LPTSTR pszCommandLine, LPCTSTR pszOutputFile, LPCTSTR pszCurrentDirectory)
+{
+	BOOL bReturn;
+	STARTUPINFO si; 
+	PROCESS_INFORMATION pi;
+	LPTSTR pszEvnVar;
+
+	if (!CreateStdioPipe()) {
+		return FALSE;
+	}
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si); 
+	ZeroMemory(&pi, sizeof(pi));
+
+	pszEvnVar = (LPTSTR)GetEnvironmentStrings();
+
+	HANDLE hFileStdOut = g_hStdout_Child_Wr;
+
+	if (pszOutputFile) {
+		SECURITY_ATTRIBUTES sa;
+		sa.nLength = sizeof(sa);
+		sa.bInheritHandle = TRUE;
+		sa.lpSecurityDescriptor = NULL;
+
+		hFileStdOut = CreateFile(pszOutputFile, GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE|FILE_SHARE_DELETE, &sa, CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL, NULL);
+		if (hFileStdOut == INVALID_HANDLE_VALUE) {
+			_tprintf(TEXT("Create output file %s failed\n"),pszOutputFile);
+			return FALSE;
+		}
+	}
+
+	si.cb = sizeof(STARTUPINFO); 
+	si.hStdError = hFileStdOut;
+	si.hStdOutput = hFileStdOut;
+	si.hStdInput = g_hStdin_Child_Rd;
+	si.dwFlags = STARTF_USESTDHANDLES;
+
+	if (bPrintLog) {
+		_tprintf(TEXT("Starting %s"),pszCommandLine);
+	}
+
+	bReturn = CreateProcess(NULL, pszCommandLine, NULL, NULL, TRUE, CREATE_NO_WINDOW | CREATE_UNICODE_ENVIRONMENT, pszEvnVar, pszCurrentDirectory, &si, &pi);
+
+	FreeEnvironmentStrings(pszEvnVar);
+
+	// Close handles to the stdin and stdout pipes no longer needed by the child process.
+	CloseHandle(g_hStdout_Child_Wr);
+	CloseHandle(g_hStdin_Child_Rd);
+
+	CloseHandle(pi.hThread);
+
+	if (hFileStdOut != g_hStdout_Child_Wr)
+		CloseHandle(hFileStdOut);
+
+	if (process_job_handle) {
+		BOOL b = AssignProcessToJobObject(process_job_handle, pi.hProcess);
+		if (!b) {
+			_tprintf(TEXT(" AssignProcessToJobObject Failed!"));
+		}
+	}
+
+	*pChildHandle = pi.hProcess;
+	*pid = pi.dwProcessId;
+
+	return TRUE;
+}
+
+static
+void SavePidToFile(LPCTSTR pszPidFile, DWORD dwPid)
+{
+	if (pszPidFile) {
+		HANDLE hFilePid = CreateFile(pszPidFile, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+		if (hFilePid != INVALID_HANDLE_VALUE) {
+			char szBuff[64];
+			//DWORD dwPid = GetProcessId(pi.hProcess);
+			DWORD dwWrite = 0;
+			int len = sprintf_s(szBuff, ARRAYSIZE(szBuff), "%u", dwPid);
+			WriteFile(hFilePid, szBuff, len, &dwWrite, NULL);
+
+			CloseHandle(hFilePid);
+		}
+	}
+}
+
+static 
+BOOL Fork()
+{
+	STARTUPINFO si; 
+	PROCESS_INFORMATION pi;
+	LPTSTR pszEvnVar;
+	BOOL bReturn;
+	HANDLE hStdOutRead, hStdoutWrite;
+	SECURITY_ATTRIBUTES saAttr; 
+	LPTSTR pszCommandLine = GetCommandLine();
+	LPTSTR pszPos = _tcsstr(pszCommandLine, TEXT("/r"));
+
+	if (!pszPos) _tcsstr(pszCommandLine, TEXT("-r"));
+
+	if (!pszPos)
+		return FALSE;
+
+	pszPos[1] = 'R';
+
+	// Set the bInheritHandle flag so pipe handles are inherited. 
+	saAttr.nLength = sizeof(SECURITY_ATTRIBUTES); 
+	saAttr.bInheritHandle = TRUE; 
+	saAttr.lpSecurityDescriptor = NULL; 
+	
+	if (CreatePipe(&hStdOutRead, &hStdoutWrite, &saAttr, 256)) {
+		SetHandleInformation(hStdOutRead, HANDLE_FLAG_INHERIT, 0);
+	}
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si); 
+	ZeroMemory(&pi, sizeof(pi));
+
+	si.hStdError = hStdoutWrite;
+	si.hStdOutput = hStdoutWrite;
+	si.hStdInput = NULL;
+	si.dwFlags = STARTF_USESTDHANDLES;
+
+	pszEvnVar = (LPTSTR)GetEnvironmentStrings();
+
+	bReturn = CreateProcess(NULL, pszCommandLine, NULL, NULL, TRUE, CREATE_NO_WINDOW | CREATE_UNICODE_ENVIRONMENT, pszEvnVar, NULL, &si, &pi);
+
+	if (bReturn) {
+#define BUFSIZE 4096
+		BYTE chBuf[BUFSIZE];
+		BOOL bSuccess;
+		DWORD dwRead = 0, dwAvail = 0;
+		DWORD dwWait = WaitForSingleObject(pi.hProcess, 200);
+
+		if (dwWait == 0)
+			bReturn = FALSE;
+
+		if (PeekNamedPipe(hStdOutRead, NULL, NULL, &dwRead, &dwAvail, NULL)) {
+			if ( dwAvail > 0) {
+				if (dwAvail > BUFSIZE)
+					dwAvail = BUFSIZE;
+
+				bSuccess = ReadFile( hStdOutRead, chBuf, dwAvail, &dwRead, NULL);
+				if (bSuccess) {
+					DWORD dwWritten;
+					HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+					WriteFile(hStdOut, chBuf, dwRead, &dwWritten, NULL);
+				}
+			}
+		}
+	}
+
+	FreeEnvironmentStrings(pszEvnVar);
+
+	CloseHandle(hStdoutWrite);
+	CloseHandle(hStdOutRead);
+
+	CloseHandle(pi.hThread);
+	CloseHandle(pi.hProcess);
+
+	return bReturn;
+}
 
 void Usage() {
 	printf("RunHiddenConsole Usage:\n"
-		"RunHiddenConsole.exe [/l] [/w] [/o output-file] commandline\n"
+		"RunHiddenConsole.exe [/l] [/w] [/r] [/n name] [/k name] [/o output-file] [/p pidfile] commandline\n"
 	 "For example:\n"
-	 "RunHiddenConsole.exe /l e:\\WNMP\\PHP\\php-cgi.exe -b 127.0.0.1:9000 -c e:\\WNMP\\php\\php.ini\n"
-	 "RunHiddenConsole.exe /l E:/WNMP/nginx/nginx.exe -p E:/WNMP/nginx\n"
-	 "The /l is optional, which means printing the result of process startup\n"
-	 "The /w is optional, which means waiting for termination of the process\n"
-	 "The /o is optional, which means redirecting the output of the program to a file\n");
+	 "RunHiddenConsole.exe /l /r e:\\WNMP\\PHP\\php-cgi.exe -b 127.0.0.1:9000 -c e:\\WNMP\\php\\php.ini\n"
+	 "RunHiddenConsole.exe /l /r E:/WNMP/nginx/nginx.exe -p E:/WNMP/nginx\n"
+	 "The /l is optional, printing the result of process startup\n"
+	 "The /w is optional, waiting for termination of the process\n"
+	 "The /o is optional, redirecting the output of the program to a file\n"
+	 "The /p is optional, saving the process id to a file\n"
+	 "The /r is optional, supervise the child process, if the child process exits, restart the child process\n"
+	 "The /n is optional, naming control signals\n"
+	 "The /k is optional, kill the daemon according to the specified control signal\n");
 }
 
-int _tmain(int _Argc, _TCHAR ** _Argv)
+int _tmain(int nArgc, _TCHAR ** ppArgv)
 {
 	TCHAR * pch;
-	TCHAR * pszExePath,szExePath[MAX_FILEPATH];
-	TCHAR szCurrentDirectory[MAX_FILEPATH];
-	TCHAR * pszCommandLine = NULL,*pszOutputFile = NULL;
+	TCHAR * pszExePath, szExePath[MAX_FILEPATH], szCurrentDirectory[MAX_FILEPATH];
+	TCHAR * pszCommandLine = NULL, *pszOutputFile = NULL, *pszPidFile = NULL, *pszSignalName = NULL;
 	BOOL bHasSpace;
 	BOOL bReturn;
-	LPTSTR pszEvnVar;
-	BOOL bWaitExit = 0,bPrintLog = 0;
-	STARTUPINFO si; 
-	PROCESS_INFORMATION pi; 
-	int  iCmdLinePos = 1,i;
+	BOOL bWaitExit = 0, bPrintLog = 0, bResume = 0, bFork = 0, bKill = 0;
+	int  iCmdLinePos = 1, i;
 	HANDLE hStdOut;
+	HANDLE hChildProcess = NULL;
+	DWORD dwChildPid = 0;
+	HANDLE hEventExit = NULL;
 
-	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	if (GetACP() == 936) {
-		setlocale(LC_ALL, "chs");
-	}
-
-	GetModuleFileName(NULL,g_szMyPath,ARRAYSIZE(g_szMyPath));
+	GetModuleFileName(NULL,g_szMyPath, ARRAYSIZE(g_szMyPath));
 
 	if (_tcsstr(g_szMyPath,TEXT("..\\"))) {
 		int nTextLength = (int)_tcslen(g_szMyPath);
-		LPTSTR pszDir = FixPathFileNameString(g_szMyPath,nTextLength);
+		LPTSTR pszDir = FixRelativePath(g_szMyPath, nTextLength);
 
 		if (pszDir) {
-			_tcscpy(g_szMyPath,pszDir);
+			_tcscpy(g_szMyPath, pszDir);
 
 			free(pszDir);
 		}
@@ -169,16 +379,16 @@ int _tmain(int _Argc, _TCHAR ** _Argv)
 	
 	g_iMyPathLen = pch - g_szMyPath;
 
-	if (_Argc < 2) {
+	if (nArgc < 2) {
 		Usage();
 		//
 		return -1;
 	}
 
-	for (i=1;i <_Argc;i++){
-		if (  (_Argv[i][0] == '-') || (_Argv[i][0] == '/') )	{
-			if (_tcslen(_Argv[i]) == 2) {
-				switch (tolower(_Argv[i][1])){
+	for (i=1;i <nArgc;i++){
+		if (  (ppArgv[i][0] == '-') || (ppArgv[i][0] == '/') )	{
+			if (_tcslen(ppArgv[i]) == 2) {
+				switch (tolower(ppArgv[i][1])){
 				case 'l':
 					bPrintLog = 1;
 					break;
@@ -187,10 +397,10 @@ int _tmain(int _Argc, _TCHAR ** _Argv)
 					bWaitExit = 1;
 					break;
 				case 'o':
-					if (i <_Argc -1) {
+					if (i <nArgc -1) {
 						i++;
 						iCmdLinePos ++;
-						pszOutputFile = _Argv[i];
+						pszOutputFile = ppArgv[i];
 					}
 					else {
 						_tprintf(TEXT("No output file!\n"));
@@ -198,6 +408,46 @@ int _tmain(int _Argc, _TCHAR ** _Argv)
 						return -1;
 					}
 					
+					break;
+				case 'p':
+					if (i <nArgc -1) {
+						i++;
+						iCmdLinePos ++;
+						pszPidFile = ppArgv[i];
+					}
+					else {
+						_tprintf(TEXT("No process id file!\n"));
+						Usage();
+						return -1;
+					}
+
+					break;
+				case 'n':
+					if (i <nArgc -1) {
+						i++;
+						iCmdLinePos ++;
+						pszSignalName = ppArgv[i];
+
+						if (_tcslen(pszSignalName) > 32) {
+							_tprintf(TEXT("The signal name is too long, it should be less than 32 characters!\n"));
+							Usage();
+							return -1;
+						}
+					}
+					else {
+						_tprintf(TEXT("No signal name specified!\n"));
+						Usage();
+						return -1;
+					}
+					break;
+				case 'r':
+					bFork = 1;  // fork a child process as deamon
+					break;
+				case 'R':
+					bResume = 1;
+					break;
+				case 'k':
+					bKill = 1;
 					break;
 				}
 			}
@@ -209,14 +459,60 @@ int _tmain(int _Argc, _TCHAR ** _Argv)
 		}
 	}
 	
-	if (iCmdLinePos >= _Argc) {
-		return -1;
-	}
-	if (!InitStdOut()) {
-		return -3;
+	if (bKill) {
+		TCHAR szSignalName[64];
+
+		if (!pszSignalName) {
+			printf("Kill without signal name.\n");
+
+			return -1;
+		}
+
+		_stprintf_s(szSignalName, ARRAYSIZE(szSignalName), TEXT("Global\\rhc_exit_%s"), pszSignalName);
+
+		hEventExit = OpenEvent(EVENT_MODIFY_STATE, FALSE, szSignalName);
+
+		if (hEventExit) {
+			SetEvent(hEventExit);
+		}
+
+		return 0;
 	}
 
-	pszExePath = _Argv[iCmdLinePos];
+	if (iCmdLinePos >= nArgc) {
+		return -1;
+	}
+
+	if (bResume && pszSignalName) {
+		TCHAR szSignalName[64];
+
+		_stprintf_s(szSignalName, ARRAYSIZE(szSignalName), TEXT("Global\\rhc_exit_%s"), pszSignalName);
+
+		hEventExit = OpenEvent(0, FALSE, szSignalName);
+
+		if (hEventExit) {
+			_tprintf(TEXT("Process with signal name %s is already existed.\n"), pszSignalName);
+
+			CloseHandle(hEventExit);
+
+			return 0;
+		}
+	}
+
+	if (bFork) {
+		if (Fork())
+			return 0;
+
+		bResume = 1;
+	}
+
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	if (GetACP() == 936) {
+		setlocale(LC_ALL, "chs");
+	}
+	
+	pszExePath = ppArgv[iCmdLinePos];
 	if (pszExePath[1] != ':') {
 		_tcscpy(szExePath,g_szMyPath);
 		_tcscpy(&szExePath[g_iMyPathLen],pszExePath);
@@ -246,97 +542,114 @@ int _tmain(int _Argc, _TCHAR ** _Argv)
 
 	pch = pszCommandLine;
 
-	bHasSpace = _tcschr(pszExePath,' ') != NULL;
+	bHasSpace = _tcschr(pszExePath, ' ') != NULL;
 
-	if (bHasSpace) *pch ++ = '\"';
+	if (bHasSpace) *pch++ = '\"';
 	_tcscpy(pch,pszExePath);
 	pch += _tcslen(pszExePath);
-	if (bHasSpace) *pch ++ = '\"';
+	if (bHasSpace) *pch++ = '\"';
 	
-	for (i = iCmdLinePos + 1; i < _Argc; i ++) {
-		_TCHAR * argv = _Argv[i];
+	for (i = iCmdLinePos + 1; i < nArgc; i ++) {
+		_TCHAR * argv = ppArgv[i];
 
-		*pch ++ = ' ';
+		*pch++ = ' ';
 
-		bHasSpace = _tcschr(argv,' ') != NULL;
+		bHasSpace = _tcschr(argv, ' ') != NULL;
 
-		if (bHasSpace) *pch ++ = '\"';
-		_tcscpy(pch,argv);
+		if (bHasSpace) *pch++ = '\"';
+		_tcscpy(pch, argv);
 		pch += _tcslen(argv);
-		if (bHasSpace) *pch ++ = '\"';
+		if (bHasSpace) *pch++ = '\"';
 	}
 	
 	*pch = 0;
 	
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(si); 
-	ZeroMemory(&pi, sizeof(pi));
-
-	pszEvnVar = (LPTSTR)GetEnvironmentStrings();
-
-	HANDLE hFileStdOut = g_hChildStd_OUT_Wr;
-
-	if (pszOutputFile) {
-		SECURITY_ATTRIBUTES sa;
-		sa.nLength = sizeof(sa);
-		sa.bInheritHandle = TRUE;
-		sa.lpSecurityDescriptor = NULL;
-		
-		hFileStdOut = CreateFile(pszOutputFile,GENERIC_WRITE,FILE_SHARE_READ|FILE_SHARE_WRITE|FILE_SHARE_DELETE,&sa,CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL,NULL);
-		if (hFileStdOut == INVALID_HANDLE_VALUE) {
-			_tprintf(TEXT("Create output file %s failed\n"),pszOutputFile);
-			return -1;
-		}
+	if (bResume) {
+		CreateChildProcessJob();
 	}
 	
-	si.cb = sizeof(STARTUPINFO); 
-	si.hStdError = hFileStdOut;
-	si.hStdOutput = hFileStdOut;
-	si.hStdInput = g_hChildStd_IN_Rd;
-	si.dwFlags = STARTF_USESTDHANDLES;
-
-	if (bPrintLog) {
-		_tprintf(TEXT("Starting %s"),pszCommandLine);
-	}
-
-	bReturn = CreateProcess(NULL, pszCommandLine, NULL, NULL, TRUE, CREATE_NO_WINDOW | CREATE_UNICODE_ENVIRONMENT, pszEvnVar, szCurrentDirectory, &si, &pi);
-	
-	FreeEnvironmentStrings(pszEvnVar);
-	free(pszCommandLine);
-
-#define DEFAULT_CONSOLE_COLOR	( FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
+	bReturn = CreateChildProcess(&hChildProcess, &dwChildPid, bPrintLog, pszCommandLine, pszOutputFile, szCurrentDirectory);
 
 	if (!bReturn) {
 		DWORD dwError = GetLastError();
 
 		if (bPrintLog) {
-			SetConsoleTextAttribute( hStdOut, FOREGROUND_RED ); 
-			_tprintf(TEXT(" Failed!"),dwError);
-			SetConsoleTextAttribute( hStdOut, DEFAULT_CONSOLE_COLOR );
+			SetConsoleTextAttribute(hStdOut, FOREGROUND_RED ); 
+			_tprintf(TEXT(" Failed!"), dwError);
+			SetConsoleTextAttribute(hStdOut, DEFAULT_CONSOLE_COLOR );
 
-			_tprintf(TEXT(",Error Code:%u\n"),dwError);
+			_tprintf(TEXT(",Error Code:%u\n"), dwError);
 		}
 
 		return -5;
 	}
 
-	CloseHandle(pi.hThread);
-
 	if (bPrintLog) {
-		SetConsoleTextAttribute( hStdOut, FOREGROUND_GREEN ); 
+		SetConsoleTextAttribute(hStdOut, FOREGROUND_GREEN ); 
 		_tprintf(TEXT(" Success!\n"));
-		SetConsoleTextAttribute( hStdOut, DEFAULT_CONSOLE_COLOR); 
+		SetConsoleTextAttribute(hStdOut, DEFAULT_CONSOLE_COLOR); 
+	}
+	if (bPrintLog) {
+		fflush(stdout);
 	}
 
-	if (bWaitExit) {
-		WaitForSingleObject(pi.hProcess,INFINITE);
+	SavePidToFile(pszPidFile, dwChildPid);
+
+	if (bWaitExit || bResume) {
+		
+		if (pszSignalName) {
+			TCHAR szSignalName[64];
+
+			_stprintf_s(szSignalName, ARRAYSIZE(szSignalName), TEXT("Global\\rhc_exit_%s"), pszSignalName);
+
+			hEventExit = CreateEvent(NULL, TRUE, FALSE, szSignalName);
+		}
+		// Supervise the child process, if the child process exits, restart the child process
+		while(TRUE) {
+			HANDLE handles[2];
+			DWORD dwWait, nCount = 1;
+						
+			handles[0] = hChildProcess;
+			if (hEventExit) {
+				handles[1] = hEventExit;
+				nCount++;
+			}
+			
+			dwWait = WaitForMultipleObjects(nCount, handles, FALSE, INFINITE);
+
+			if (dwWait == WAIT_OBJECT_0) {
+				CloseHandle(hChildProcess);
+
+				if (!bResume)
+					break;
+
+				bReturn = CreateChildProcess(&hChildProcess, &dwChildPid, FALSE, pszCommandLine, pszOutputFile, szCurrentDirectory);
+
+				if (bReturn)
+					SavePidToFile(pszPidFile, dwChildPid);
+				else
+					break;
+			}
+			else if (dwWait == WAIT_OBJECT_0 + 1) {
+				// Received exit signal
+				break;
+			}
+			else {
+				// System error
+				break;
+			}
+		}
 	}
 
-	CloseHandle(pi.hProcess);
+	free(pszCommandLine);
 
-	if (hFileStdOut != g_hChildStd_OUT_Wr)
-		CloseHandle(hFileStdOut);
-	
+	if (hEventExit) {
+		CloseHandle(hEventExit);
+	}
+
+	DestroyChildProcessJob();
+			
 	return 0;
 }
+
 
